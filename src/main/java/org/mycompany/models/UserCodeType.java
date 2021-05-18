55417b18bc4f7e8049dc0f0873422bb8e6214b60
Ajay Borgave <ajayborgave.ab@gmail.com>
2021-05-18 17:46:58 +0530

    Initial commit

---
+++ src/main/java/org/mycompany/models/UserCodeType.java
@@ -0,0 +1,179 @@
+package org.mycompany.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="user_code")
+public class UserCodeType {
+		@Id
+		@GeneratedValue(strategy = GenerationType.AUTO)
+	    protected int id;
+		
+		@Column(name="user_code1")
+		protected String userCode1;
+		@Column(name="user_code2")
+		protected String userCode2;
+		@Column(name="user_code3")
+	    protected String userCode3;
+		@Column(name="user_code4")
+	    protected String userCode4;
+		@Column(name="user_code5")
+	    protected String userCode5;
+		@Column(name="user_code6")
+	    protected String userCode6;
+		@Column(name="user_code7")
+	    protected String userCode7;
+		@Column(name="user_code8")
+	    protected String userCode8;
+		@Column(name="user_code9")
+	    protected String userCode9;
+		@Column(name="user_code10")
+	    protected String userCode10;
+		@Column(name="user_code11")
+	    protected String userCode11;
+		@Column(name="user_code12")
+	    protected String userCode12;
+		@Column(name="user_code13")
+	    protected String userCode13;
+		@Column(name="user_code14")
+	    protected String userCode14;
+		@Column(name="user_code15")
+	    protected String userCode15;
+		@Column(name="user_code16")
+	    protected String userCode16;
+		@Column(name="user_code17")
+	    protected String userCode17;
+		@Column(name="user_code18")
+	    protected String userCode18;
+		@Column(name="user_code19")
+	    protected String userCode19;
+		@Column(name="user_code20")
+	    protected String userCode20;
+		public String getUserCode1() {
+			return userCode1;
+		}
+		public void setUserCode1(String userCode1) {
+			this.userCode1 = userCode1;
+		}
+		public String getUserCode2() {
+			return userCode2;
+		}
+		public void setUserCode2(String userCode2) {
+			this.userCode2 = userCode2;
+		}
+		public String getUserCode3() {
+			return userCode3;
+		}
+		public void setUserCode3(String userCode3) {
+			this.userCode3 = userCode3;
+		}
+		public String getUserCode4() {
+			return userCode4;
+		}
+		public void setUserCode4(String userCode4) {
+			this.userCode4 = userCode4;
+		}
+		public String getUserCode5() {
+			return userCode5;
+		}
+		public void setUserCode5(String userCode5) {
+			this.userCode5 = userCode5;
+		}
+		public String getUserCode6() {
+			return userCode6;
+		}
+		public void setUserCode6(String userCode6) {
+			this.userCode6 = userCode6;
+		}
+		public String getUserCode7() {
+			return userCode7;
+		}
+		public void setUserCode7(String userCode7) {
+			this.userCode7 = userCode7;
+		}
+		public String getUserCode8() {
+			return userCode8;
+		}
+		public void setUserCode8(String userCode8) {
+			this.userCode8 = userCode8;
+		}
+		public String getUserCode9() {
+			return userCode9;
+		}
+		public void setUserCode9(String userCode9) {
+			this.userCode9 = userCode9;
+		}
+		public String getUserCode10() {
+			return userCode10;
+		}
+		public void setUserCode10(String userCode10) {
+			this.userCode10 = userCode10;
+		}
+		public String getUserCode11() {
+			return userCode11;
+		}
+		public void setUserCode11(String userCode11) {
+			this.userCode11 = userCode11;
+		}
+		public String getUserCode12() {
+			return userCode12;
+		}
+		public void setUserCode12(String userCode12) {
+			this.userCode12 = userCode12;
+		}
+		public String getUserCode13() {
+			return userCode13;
+		}
+		public void setUserCode13(String userCode13) {
+			this.userCode13 = userCode13;
+		}
+		public String getUserCode14() {
+			return userCode14;
+		}
+		public void setUserCode14(String userCode14) {
+			this.userCode14 = userCode14;
+		}
+		public String getUserCode15() {
+			return userCode15;
+		}
+		public void setUserCode15(String userCode15) {
+			this.userCode15 = userCode15;
+		}
+		public String getUserCode16() {
+			return userCode16;
+		}
+		public void setUserCode16(String userCode16) {
+			this.userCode16 = userCode16;
+		}
+		public String getUserCode17() {
+			return userCode17;
+		}
+		public void setUserCode17(String userCode17) {
+			this.userCode17 = userCode17;
+		}
+		public String getUserCode18() {
+			return userCode18;
+		}
+		public void setUserCode18(String userCode18) {
+			this.userCode18 = userCode18;
+		}
+		public String getUserCode19() {
+			return userCode19;
+		}
+		public void setUserCode19(String userCode19) {
+			this.userCode19 = userCode19;
+		}
+		public String getUserCode20() {
+			return userCode20;
+		}
+		public void setUserCode20(String userCode20) {
+			this.userCode20 = userCode20;
+		}
+	    
+	    
+}
